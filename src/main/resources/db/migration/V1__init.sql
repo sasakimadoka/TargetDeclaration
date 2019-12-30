ca90219655393cef61b5578a86ceea036a6faad0
madsasak <madsasak@yahoo-corp.jp>
2019-12-30 20:38:04 +0900

    create文追加

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE targets (
+  id int(255) NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  expired_at datetime ,
+  status int(255) NOT NULL,
+  is_deleted int(255) NOT NULL,
+  created_at datetime default current_timestamp NOT NULL,
+  updated_at timestamp default current_timestamp on update current_timestamp NOT NULL,
+  color_label varchar(255),
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
